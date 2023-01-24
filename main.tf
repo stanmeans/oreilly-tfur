c1b053825744f82069ccac9f669efc54dcfeb249
stanmeans <stan.i.means@hotmail.com>
2023-01-23 20:22:01 -0500

    added ASG

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                     = "ami-0fb653ca2d3203ac1"
-  instance_type           = "t2.micro"
-  vpc_security_group_ids  = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  ami             = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -13,10 +13,23 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
   
-  user_data_replace_on_change = true
+  # Required when using a launch configuration with an autoscaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    "name" = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration  = aws_launch_configuration.example.name
+  vpc_zone_identifier   = data.aws_subnets.default.id
+
+  min_size=2
+  max_size = 10
+
+  tag {
+    key                 =  "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -30,6 +43,20 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name    = "vpc-id"
+    values  = [data.aws_vpc.default.id]
+  }
+}
+
+
+
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
